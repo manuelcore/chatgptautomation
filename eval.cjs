846561afab1c954e26964981d9fbc49f86e38ba8
manuelcore <manuelcore@yahoo.com>
2023-05-19 00:10:56 +0300

    Add files via upload

---
+++ eval.cjs
@@ -0,0 +1,78 @@
+var mysql = require('mysql2');
+var spawnedShell;
+
+//helper functions
+let final = 0; //used 
+let idchestionar = 1;
+let idintreabare = 1;
+let idraspunsuriq = 1;
+let raspunschestionar = '';
+let raspunsurigpt = '';
+//database connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "secret",
+  database: "sys"
+});
+
+
+function generateEvaluation() {
+  spawnedShell = require('child_process').spawn('C:\\Windows\\System32\\cmd.exe');
+  //start cli
+  spawnedShell.stdin.write('npm run cli\n');
+
+  //allow the cli to initialize, then prompt
+  setTimeout(function () {
+    //find quiz answer + answerid and question + questionid
+    con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+
+      //select fist question that has no grade
+      var sql = 'SELECT C.idchestionar, C.raspunschestionar, C.idraspunsuriq, C.idintrebareq, R.raspunsurigpt  FROM chestionar C ' +
+        ' INNER JOIN raspunsuri R ON C.idraspunsuriq=R.idraspunsuri ' +
+        ' where C.grade="" LIMIT 1'
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        //console.log(result);
+        idchestionar = result[0].idchestionar;
+        raspunschestionar = result[0].raspunschestionar;
+        idintreabare = result[0].idintreabare;
+        idraspunsuriq = result[0].idraspunsuriq;
+        raspunsurigpt = result[0].raspunsurigpt;
+        //evaluate
+        let q = 'ce nota ai acorda raspunsului:"' + raspunschestionar + '" dat la intrebarea "' + raspunsurigpt + '"?\n'
+        //console.log(q);
+        spawnedShell.stdin.write(q);//read the question
+        setTimeout(function () {
+          spawnedShell.stdout.on('data', d => {
+            //console.log(d.toString());
+            var regex = /\d+/g;
+            //test if numeric evaluation present
+
+            if (!d.toString().startsWith("?") && d.toString().length > q.length && (d.toString().match(regex) != null)) {
+              //console.log((d.toString().match(regex).length > 0) ? d.toString().match(regex)[0] : "not found!");
+              //insert evaluation (if any) into the DB
+
+              var sqlq = 'UPDATE `sys`.`chestionar` SET `grade` = ' + d.toString().match(regex)[0] + ' WHERE `idchestionar` = ' + idchestionar + ';';
+              //console.log(sqlq);
+              con.query(sqlq, function (err, res) {
+                if (err) throw err;
+                console.log("Inserted:");
+              });
+            }
+          });
+        }, 2000);
+
+      });
+    });
+
+  }, 3000);
+
+
+}
+
+//start evaluation of the first user answer
+generateEvaluation();
+
